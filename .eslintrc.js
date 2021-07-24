bf5e56d5a40e5fbdbce0cb2308c74a24b3f20f63
khanhth99 <khanhth99@gmail.com>
2021-07-24 11:01:06 +0700

    add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,10 +29,10 @@ module.exports = {
 			'error',
 			'tab'
 		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
+		// 'linebreak-style': [
+		// 	'error',
+		// 	'windows'
+		// ],
 		'quotes': [
 			'error',
 			'single'
