ae0d1e1f1f9cceea01e540ae8864c8afd4930e6d
anh_quan <anhqu@DESKTOP-2308CTT>
2021-08-05 10:17:58 +0700

    first-commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef} from 'react';
-import { 
+import React, { useEffect, useRef } from 'react';
+import {
 	Container,
 	Row,
 	Image,
@@ -13,22 +13,22 @@ import Logo from '../../assets/images/lg.png';
 import { sideBarHide, sideBarShow } from '../../redux/actions/navbarAction';
 import styles from './navbarStyle.module.css';
 import { useTranslation } from 'react-i18next';
-import { useSelector, useDispatch  } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import OutsideClick from '../../helpers/outsideClick';
 function Navbar() {
 	const boxRef = useRef(null);
 	const boxOutsideClick = OutsideClick(boxRef);
 	// console.log('boxOutsideClick',boxOutsideClick);
-	
+
 	// const [sidebar, setSidebar] = useState(false);
 	const sidebar = useSelector(state => state.navbarReducer.shownavbar);
 	const dispatch = useDispatch();
 	const { t } = useTranslation();
 	useEffect(() => {
-		if (boxOutsideClick){
+		if (boxOutsideClick) {
 			dispatch(sideBarHide(false));
 		}
-	},[boxOutsideClick, dispatch]);
+	}, [boxOutsideClick, dispatch]);
 	const showSidebar = () => {
 		dispatch(sideBarShow(true));
 	};
@@ -39,14 +39,14 @@ function Navbar() {
 		<div ref={boxRef}>
 			<Container fluid>
 				<Row className={styles.headerTop}>
-					<div  className={styles.buttonSidebars}><button onClick={() => showSidebar()} className={styles.buttonSidebar}><CgMenu /></button></div>
+					<div className={styles.buttonSidebars}><button onClick={() => showSidebar()} className={styles.buttonSidebar}><CgMenu /></button></div>
 					<div className={styles.title}>
 						{/* <Image style={{ width: '40px' }} src={Logo} alt="" roundedCircle /> */}
 						{t('Translate.title')}
 					</div>
 				</Row>
 			</Container>
-			<nav className={sidebar ? [styles.nav_menu,styles.active].join(' ') : styles.nav_menu}>
+			<nav className={sidebar ? [styles.nav_menu, styles.active].join(' ') : styles.nav_menu}>
 				<ul className={styles.nav_menu_items}>
 					<li className={styles.logo}>
 						<div className={styles.logosub}>
