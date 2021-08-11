636231f651ac253d96eb808f7664d70b7f88a0bc
Đức Khải <nguyenduckhai1998@gmail.com>
2021-08-11 17:24:09 +0700

    fix: history UI

--- src/screens/HistoryAndFavorite/index.js
+++ src/screens/HistoryAndFavorite/index.js
@@ -1,18 +1,37 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState, forwardRef } from 'react';
 import styles from './historyAndFavorite.module.css';
-import { ButtonToolbar } from 'react-bootstrap';
-import { MdMoreVert, MdArrowForward, MdSearch, MdStarBorder, MdContentCopy, MdVolumeUp } from 'react-icons/md';
+import { ButtonToolbar, Dropdown } from 'react-bootstrap';
+import { MdMoreVert, MdArrowForward, MdStarBorder, MdContentCopy, MdVolumeUp } from 'react-icons/md';
 
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+const history = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+const favorite = [17, 18 ];
 
 export default function HistoryAndFavorite() {
-	// eslint-disable-next-line react/prop-types
-	const Item = () => {
+	const [option, setOption] = useState('history');
+	const [shownId, setShownId] = useState(1);
+
+	// eslint-disable-next-line react/display-name
+	const CustomToggle = forwardRef(({ children, onClick }, ref) => (
+		<button
+		  className={styles.buttonDropDown}
+		  href=""
+		  ref={ref}
+		  onClick={(e) => {
+				e.preventDefault();
+				onClick(e);
+		  }}
+		>
+		  {children}
+		  {/* &#x25bc; This is arrow dropdown */}
+		</button>
+	  ));
+	  
+	const Item = ({id}) => {
 		return (
 			<div style={{height: 166, width: '100%', color: 'white', display: 'flex'}}>
-				<div className={styles.buttonListChosen}>
+				<div className={id === shownId ? styles.buttonListChosenActive : styles.buttonListChosen}>
 					{/* {number} */}
 					<div style={{flex: 0.9, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', fontSize: 25}}>
 						<div>
@@ -22,8 +41,23 @@ export default function HistoryAndFavorite() {
 						<div style={{color: '#A3A3A3', fontSize: 22}}>Trung <MdArrowForward style={{marginLeft: 10, marginRight: 10}}/> Viet</div>
 					</div>
 					<div style={{flex: 0.1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
-						<MdMoreVert size={25} onClick={() => {}}/>
-						<MdArrowForward size={25} onClick={() => {}}/>
+						{option === 'history' ? 
+							<Dropdown >
+								<Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+									<MdMoreVert size={25} />
+								</Dropdown.Toggle>
+								<Dropdown.Menu>
+									<Dropdown.Item >Thêm vào từ vựng</Dropdown.Item>
+									<Dropdown.Item >Xóa</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown> : 
+							<button className={styles.buttonDropDown}>
+								<MdStarBorder size={25}/>
+							</button>
+						}
+						<button className={styles.buttonDropDown} onClick={() => setShownId(id)}>
+							<MdArrowForward size={25}/>
+						</button>
 					</div>
 				</div>
 			</div>
@@ -35,28 +69,28 @@ export default function HistoryAndFavorite() {
 			<div className={styles.innerBox}>
 				<div className={styles.tabBox}>
 					<ButtonToolbar>
-						<button className={styles.tab}>LỊCH SỬ</button>
-						<button className={styles.tab}>TỪ VỰNG</button>
+						<button className={option === 'history'? styles.tabActive : styles.tab} onClick={() => setOption('history')}>LỊCH SỬ</button>
+						<button className={option === 'favorite'? styles.tabActive : styles.tab} onClick={() => setOption('favorite')}>TỪ VỰNG</button>
 					</ButtonToolbar>
 				</div>
 				<div className={styles.innerBody}>
 					<div className={styles.innerBodyLeft}>
 						<div className={styles.innerSearchBox}>
-							{/* <input type='text' className={styles.inputSearch} placeholder="Enter email" /> */}
-							<MdSearch size={30} />
-							<div className="dropdown">
-								<button className={styles.buttonDropDown} type="button" id="dropdownMenuButton" data-toggle="dropdown" >
+							<input type='text' className={styles.inputSearch} placeholder="Tìm kiếm"/>
+							{/* <MdSearch size={30} /> */}
+							<Dropdown >
+								<Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
 									<MdMoreVert size={30} />
-								</button>
-								<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-									<div className="dropdown-item" href="#">Action</div>
-									<div className="dropdown-item" href="#">Another action</div>
-									<div className="dropdown-item" href="#">Something else here</div>
-								</div>
-							</div>
+								</Dropdown.Toggle>	
+								<Dropdown.Menu>
+									<Dropdown.Item >Xóa tất cả</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
 						</div>
-						<div className="form-select" size="5" style={{flex: 1, overflow: 'auto'}}>
-							{numbers.map((number) => <Item key={number.toString()}/>)}
+						<div className="form-select" style={{flex: 1, overflow: 'auto'}}>
+							{option === 'history' ? 
+								history.map((number) => <Item key={number.toString()} id={number}/>) : 
+								favorite.map((number) => <Item key={number.toString()} id={number}/>)}
 						</div>
 					</div>
 					<div className={styles.innerBodyRight}>
@@ -64,23 +98,35 @@ export default function HistoryAndFavorite() {
 							<div style={{fontWeight: 500, fontSize: 22, textTransform: 'uppercase' }}>
 								Trung <MdArrowForward style={{marginLeft: 10, marginRight: 10}}/> Viet
 							</div>
-							<div>
-								<MdStarBorder size={30} style={{marginRight: 10}}/>
-								<MdArrowForward size={30} />
+							<div style={{display: 'flex'}}>
+								<button className={styles.buttonDropDown}>
+									<MdStarBorder size={30}/>
+								</button>
+								<button className={styles.buttonDropDown}>
+									<MdArrowForward size={30} />
+								</button>
 							</div>
 						</div>
 						<div className={styles.innerBodyRightBox} style={{marginBottom: 40}}>
 							Text 1
 							<div style={{marginTop: 30, display: 'flex', justifyContent: 'flex-end'}}>
-								<MdContentCopy size={25} style={{marginRight: 5}}/>
-								<MdVolumeUp size={25} />
+								<button className={styles.buttonDropDown}>
+									<MdContentCopy size={25}/>
+								</button>
+								<button className={styles.buttonDropDown}>
+									<MdVolumeUp size={25} />
+								</button>
 							</div>
 						</div>
 						<div className={styles.innerBodyRightBox}>
 							Text 2
 							<div style={{marginTop: 30, display: 'flex', justifyContent: 'flex-end'}}>
-								<MdContentCopy size={25} style={{marginRight: 5}}/>
-								<MdVolumeUp size={25} />
+								<button className={styles.buttonDropDown}>
+									<MdContentCopy size={25}/>
+								</button>
+								<button className={styles.buttonDropDown}>
+									<MdVolumeUp size={25} />
+								</button>
 							</div>
 						</div>
 					</div>
