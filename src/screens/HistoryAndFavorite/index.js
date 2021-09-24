b2f6de1897c58ad5ada887d236fb2b2cb2ac8974
khaidnguyen12345@gmail.com <khaidnguyen12345@gmail.com>
2021-09-24 16:10:37 +0700

    fix: UI translation and history

--- src/screens/HistoryAndFavorite/index.js
+++ src/screens/HistoryAndFavorite/index.js
@@ -1,10 +1,28 @@
-/* eslint-disable react/prop-types */
-import React, { useState, forwardRef } from 'react';
+import React, { useState } from 'react';
 import styles from './historyAndFavorite.module.css';
-import { ButtonToolbar, Dropdown } from 'react-bootstrap';
+import { 
+	Tab, 
+	Tabs, 
+	Menu, 
+	MenuItem, 
+	Typography, 
+	Card, 
+	CardContent, 
+	CardActionArea,
+	CardActions,
+	Divider,
+} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
 // import { IS_AUTH } from '../../constants/envVar';
 import FeedBack from './components/ModalFeedBack';
-import { MdMoreVert, MdArrowForward, MdStarBorder, MdContentCopy, MdVolumeUp } from 'react-icons/md';
+import { useTranslation } from 'react-i18next';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DeleteIcon from '@mui/icons-material/Delete';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import { MdArrowForward, MdStarBorder, MdContentCopy, MdVolumeUp } from 'react-icons/md';
 
 
 const history = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
@@ -15,59 +33,47 @@ export default function HistoryAndFavorite() {
 	// 	isAuthenticated: sessionStorage.getItem(IS_AUTH),
 	// };
 	// const [modelShown, setModelShown] = useState(!fakeAuth.isAuthenticated);
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+
 	const [modelShown, setModelShown] = useState(false);
 	const [option, setOption] = useState('history');
-	const [shownId, setShownId] = useState(1);
+	// const [shownId, setShownId] = useState(1);
+	const { t } = useTranslation();
 
-	// eslint-disable-next-line react/display-name
-	const CustomToggle = forwardRef(({ children, onClick }, ref) => (
-		<button
-		  className={styles.buttonDropDown}
-		  href=""
-		  ref={ref}
-		  onClick={(e) => {
-				e.preventDefault();
-				onClick(e);
-		  }}
-		>
-		  {children}
-		  {/* &#x25bc; This is arrow dropdown */}
-		</button>
-	  ));
-	  
-	const Item = ({id}) => {
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+
+	// eslint-disable-next-line no-unused-vars
+	const NewItem = () => {
 		return (
-			<div style={{height: 166, width: '100%', color: 'white', display: 'flex'}}>
-				<div className={id === shownId ? styles.buttonListChosenActive : styles.buttonListChosen}>
-					{/* {number} */}
-					<div style={{flex: 0.9, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', fontSize: 25}}>
-						<div>
-							<div>text 1</div>
-							<div>text 2</div>
-						</div>
-						<div style={{color: '#A3A3A3', fontSize: 22}}>Trung <MdArrowForward style={{marginLeft: 10, marginRight: 10}}/> Viet</div>
-					</div>
-					<div style={{flex: 0.1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
-						{option === 'history' ? 
-							<Dropdown >
-								<Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-									<MdMoreVert size={25} />
-								</Dropdown.Toggle>
-								<Dropdown.Menu>
-									<Dropdown.Item >Thêm vào từ vựng</Dropdown.Item>
-									<Dropdown.Item >Xóa</Dropdown.Item>
-								</Dropdown.Menu>
-							</Dropdown> : 
-							<button className={styles.buttonDropDown}>
-								<MdStarBorder size={25}/>
-							</button>
-						}
-						<button className={styles.buttonDropDown} onClick={() => setShownId(id)}>
-							<MdArrowForward size={25}/>
-						</button>
-					</div>
-				</div>
-			</div>
+			<Card variant="outlined" style={{borderRadius: 0}}>
+				<CardActionArea>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="div"> 
+            				Text 1
+						</Typography>
+						<Typography gutterBottom variant="h5" component="div" color="text.secondary">
+            				Text 2
+						</Typography>
+						<Typography variant="body2" color="text.secondary">Trung <ArrowForwardIcon sx={{fontSize: 20}}/> Viet</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions sx={{p: 0}} disableSpacing style={{justifyContent: 'end'}}>
+					<IconButton aria-label="directions" id="basic-button">
+						<StarBorderIcon />
+					</IconButton>
+					<IconButton aria-label="directions" id="basic-button">
+						<DeleteIcon />
+					</IconButton>
+				</CardActions>
+			</Card>
 		);
 	};
 
@@ -75,29 +81,49 @@ export default function HistoryAndFavorite() {
 		<div style={{ display: 'flex', flex: 1, backgroundColor: 'white', height:'94vh' }}>
 			<div className={styles.innerBox}>
 				<div className={styles.tabBox}>
-					<ButtonToolbar>
-						<button className={option === 'history'? styles.tabActive : styles.tab} onClick={() => setOption('history')}>LỊCH SỬ</button>
-						<button className={option === 'favorite'? styles.tabActive : styles.tab} onClick={() => setOption('favorite')}>TỪ VỰNG</button>
-					</ButtonToolbar>
+					<Tabs Text
+						value={option}
+						onChange={(event, newValue) => setOption(newValue)}
+						variant="scrollable"
+						scrollButtons="auto"
+					>
+						<Tab label={t('History.lichsu')} value={'history'} style={{fontWeight: 'bold'}}/>
+						<Tab label={t('History.tuvung')} value={'favorite'} style={{fontWeight: 'bold'}}/>
+					</Tabs>
 				</div>
 				<div className={styles.innerBody}>
 					<div className={styles.innerBodyLeft}>
 						<div className={styles.innerSearchBox}>
-							<input type='text' className={styles.inputSearch} placeholder="Tìm kiếm"/>
-							{/* <MdSearch size={30} /> */}
-							<Dropdown >
-								<Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-									<MdMoreVert size={30} />
-								</Dropdown.Toggle>	
-								<Dropdown.Menu>
-									<Dropdown.Item >Xóa tất cả</Dropdown.Item>
-								</Dropdown.Menu>
-							</Dropdown>
+							<InputBase
+								sx={{ ml: 1, flex: 1 }}
+								placeholder="Tìm kiếm"
+								inputProps={{ 'aria-label': 'Tìm kiếm' }}
+							/>
+							<IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+								<SearchIcon />
+							</IconButton>
+							<Divider sx={{ height: '100%' }} orientation="vertical" />
+							<IconButton color="primary" sx={{ p: '10px' }} aria-label="directions" id="basic-button" onClick={handleClick}>
+								<MoreVertIcon />
+							</IconButton>
+							<Menu
+								id="basic-menu"
+								anchorEl={anchorEl}
+								open={open}
+								onClose={handleClose}
+								MenuListProps={{
+									'aria-labelledby': 'basic-button',
+								}}
+							>
+								<MenuItem onClick={handleClose}>Profile</MenuItem>
+								<MenuItem onClick={handleClose}>My account</MenuItem>
+								<MenuItem onClick={handleClose}>Logout</MenuItem>
+							</Menu>
 						</div>
 						<div className="form-select" style={{flex: 1, overflow: 'auto'}}>
 							{option === 'history' ? 
-								history.map((number) => <Item key={number.toString()} id={number}/>) : 
-								favorite.map((number) => <Item key={number.toString()} id={number}/>)}
+								history.map((number) => <NewItem key={number.toString()} id={number}/>) : 
+								favorite.map((number) => <NewItem key={number.toString()} id={number}/>)}
 						</div>
 					</div>
 					<div className={styles.innerBodyRight}>
