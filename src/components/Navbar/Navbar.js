61e5418615349755b3aaae30b2953af03c2f65cb
khaidnguyen12345@gmail.com <khaidnguyen12345@gmail.com>
2021-09-29 14:33:24 +0700

    feat: add login

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-constant-condition */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useEffect, useRef } from 'react';
 import {
 	Container,
@@ -20,18 +17,18 @@ import styles from './navbarStyle.module.css';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
 import OutsideClick from '../../helpers/outsideClick';
-// import { useGoogleLogout } from 'react-google-login';
+import { useGoogleLogout } from 'react-google-login';
 import Modal from '../Modal';
 import { IS_AUTH } from '../../constants/envVar';
 
-// const clientId =
-// 	'1006597644137-plgvccnt0d3keaojro5q3j69vkjudfvs.apps.googleusercontent.com';
+const clientId =
+	'1006597644137-plgvccnt0d3keaojro5q3j69vkjudfvs.apps.googleusercontent.com';
 
 function Navbar() {
 	const boxRef = useRef(null);
 	const boxOutsideClick = OutsideClick(boxRef);
 	const [modalShow, setModalShow] = React.useState(false);
-	// const path = window.location.pathname;
+	const path = window.location.pathname;
 
 	const fakeAuth = {
 		isAuthenticated: sessionStorage.getItem(IS_AUTH),
@@ -54,39 +51,28 @@ function Navbar() {
 		dispatch(sideBarHide(false));
 	};
 
-	// const onFailure = (res) => {
-	// 	console.log('Login failed: res:', res);
-	// 	alert(
-	// 		'Đăng nhập thất bại'
-	// 	);
-	// };
+	// eslint-disable-next-line no-unused-vars
+	const onFailure = (res) => {
+		alert(
+			'Đăng nhập thất bại'
+		);
+	};
 
-	// const onLogoutSuccess = () => {
-	// 	alert('Đăng xuất thành công');
-	// 	sessionStorage.clear();
-	// 	window.location.replace('/login');
-	// };
+	const onLogoutSuccess = () => {
+		alert('Đăng xuất thành công');
+		sessionStorage.clear();
+		window.location.replace('/login');
+	};
 
-	// const { signOut } = useGoogleLogout({
-	// 	clientId,
-	// 	onLogoutSuccess,
-	// 	onFailure,
-	// 	isSignedIn: false,
-	// 	accessType: 'offline',
-	// });
+	const { signOut } = useGoogleLogout({
+		clientId,
+		onLogoutSuccess,
+		onFailure,
+		isSignedIn: false,
+		accessType: 'offline',
+	});
 
 	return (
-		// <div ref={boxRef} style={{display: 'flex', height: '6vh'}}>
-		// 	<div style={{display: 'flex', flex: 1, backgroundColor:' #1d5193', color: 'white', flexDirection: 'row'}}>
-		// 		<div style={{position: 'relative', width:50 }}>
-		// 			<button onClick={() => showSidebar()} className={styles.buttonSidebar}>
-		// 				<CgMenu />
-		// 			</button>
-		// 		</div>
-		// 		<div style={{flex: 1, fontSize: 28, alignSelf:'center',}}>
-		// 			{t('Translate.title')}
-		// 		</div>
-		// 	</div>
 		// 	<nav className={sidebar ? [styles.nav_menu,styles.active].join(' ') : styles.nav_menu}>
 		<div ref={boxRef}>
 			<Container fluid>
@@ -105,18 +91,17 @@ function Navbar() {
 								<Dropdown.Menu>
 									<Dropdown.Item onClick={() => setModalShow(true)}>Chỉnh sửa thông tin</Dropdown.Item>
 									<Dropdown.Divider />
-									<Dropdown.Item onClick={() => {}}>Đăng xuất</Dropdown.Item>
+									<Dropdown.Item onClick={() => signOut()}>Đăng xuất</Dropdown.Item>
 								</Dropdown.Menu>
 							</Dropdown>
 						) : (
-							null
-							// path == ('/login' || '/forgot-password' || '/register') ? (
-							// 	null
-							// ) : (
-							// 	<a href="/login" style={{ color: '#fff', alignSelf: 'center', marginRight: 20 }}>
-							// 		Đăng nhập
-							// 	</a>
-							// )
+							path == ('/login' || '/forgot-password' || '/register') ? (
+								null
+							) : (
+								<a href="/login" style={{ color: '#fff', alignSelf: 'center', marginRight: 20 }}>
+							 		Đăng nhập
+							 	</a>
+							 )
 						)}
 						<Modal
 							show={modalShow}
@@ -142,7 +127,7 @@ function Navbar() {
 					})}
 				</ul>
 			</nav>
-		</div >
+		</div>
 	);
 }
 
--- src/screens/Login/LoginGoogle.js
+++ src/screens/Login/LoginGoogle.js
@@ -1,49 +1,46 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 import React from 'react';
 import { useGoogleLogin } from 'react-google-login';
 import { IS_AUTH } from '../../constants/envVar';
 
 // refresh token
-import { refreshTokenSetup } from './refreshToken';
+// import { refreshTokenSetup } from './refreshToken';
 
 const clientId =
 	'1006597644137-plgvccnt0d3keaojro5q3j69vkjudfvs.apps.googleusercontent.com';
 
 function LoginHooks() {
-	// const onSuccess = (res) => {
-	// 	// console.log('🚀 ~ file: LoginGoogle.js ~ line 12 ~ onSuccess ~ res', res);
-	// 	// console.log('Login Success: currentUser:', res.profileObj);
-	// 	alert(
-	// 		`Đăng nhập thành công chào mừng ${res.profileObj.name}.`
-	// 	);
-	// 	refreshTokenSetup(res);
-	// 	sessionStorage.setItem(IS_AUTH, res);
-	// 	if (res.profileObj) {
-	// 		window.location.replace('/');
-	// 	}
-	// };
+	const onSuccess = (res) => {
+		console.log('Login Success: currentUser:', res.accessToken);
+		alert(
+			`Đăng nhập thành công chào mừng ${res.profileObj.name}.`
+		);
+		// refreshTokenSetup(res);
+		sessionStorage.setItem(IS_AUTH, res);
+		if (res.profileObj) {
+			window.location.replace('/');
+		}
+	};
 
-	// const onFailure = (res) => {
-	// 	console.log('Login failed: res:', res);
-	// 	alert(
-	// 		'Đăng nhập thất bại'
-	// 	);
-	// };
+	// eslint-disable-next-line no-unused-vars
+	const onFailure = (res) => {
+		alert(
+			'Đăng nhập thất bại'
+		);
+	};
 
-	// const { signIn } = useGoogleLogin({
-	// 	onSuccess,
-	// 	onFailure,
-	// 	clientId,
-	// 	isSignedIn: false,
-	// 	accessType: 'offline',
-	// 	// responseType: 'code',
-	// 	// prompt: 'consent',
-	// });
+	const { signIn } = useGoogleLogin({
+		onSuccess,
+		onFailure,
+		clientId,
+		isSignedIn: false,
+		accessType: 'offline',
+		// responseType: 'code',
+		// prompt: 'consent',
+	});
 
 	return (
 		<>
-			<button onClick={() => {}} style={{ backgroundColor: '#EA4335', borderRadius: 10 }} className="btn btn-danger btn-block">
+			<button onClick={() => signIn()} style={{ backgroundColor: '#EA4335', borderRadius: 10 }} className="btn btn-danger btn-block">
 				<i className="fab fa-google fa-fw" /> Đăng nhập với Google
 			</button>
 
